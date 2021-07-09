8a6af667e9a1483f91ee8d72b5da6703dd57d68f
Tristan Sutton <tsutton@pop-os.localdomain>
2021-07-09 15:38:32 +1000

    first

---
+++ bin/github_ssh_keygen.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+import 'package:dcli/dcli.dart';
+
+void main(List<String> arguments) {
+  var HOME = Platform.environment['HOME'];
+  var email = ask(green('Enter your github email: \n'));
+  'ssh-keygen -t ed25519 -C \"$email\" -f "$HOME/.ssh/id_ed25519"'.start();
+  'ssh-agent -s'.start(runInShell: true);
+  'ssh-add ~/.ssh/id_ed25519'.start();
+  print(green('SSH public key:\n'));
+  File('$HOME/.ssh/id_ed25519.pub').readAsLines().then((value) => {
+        value.forEach((element) {
+          print('$element\n');
+        })
+      });
+}
